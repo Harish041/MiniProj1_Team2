7aaa0a5d6064bf99f0193f4c35b31ac82673adbd
Harish S <80798474+Harish041@users.noreply.github.com>
2022-11-16 11:25:38 +0530

    Add files via upload

---
+++ miniprojtablecreate.sql
@@ -0,0 +1,14 @@
+Create table Student(
+ID int primary key not null,
+Name varchar(30),
+dob date)
+
+Create table Course(
+cid int primary key not null,
+name varchar(20),
+duration varchar(10),
+fee numeric(10,2))
+
+
+
+Select*from Course
