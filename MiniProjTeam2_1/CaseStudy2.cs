0adf547d13564f7719252ee24fa09ff7ef78efb3
Harish S <80798474+Harish041@users.noreply.github.com>
2022-11-18 09:01:11 +0530

    Separate files into single folder

---
+++ MiniProjTeam2_1/CaseStudy2.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProjTeam2_1
+{
+    class CaseStudy2
+    {
+    }
+
+    public class Course
+    {
+        public int cid { get; set; }
+        public string name { get; set; }
+        public string duration { get; set; }
+        public float fee { get; set; }
+    }
+
+    public class Enroll
+    {
+        private Student student;
+        private Course course;
+        private DateTime EnrollmentDate;
+
+        public Student std
+        {
+            get { return student; }
+            set { student = value; }
+        }
+        public Course crs
+        {
+            get { return course; }
+            set { course = value; }
+        }
+        public DateTime enrollDate
+        {
+            get { return EnrollmentDate; }
+            set { EnrollmentDate = value; }
+        }
+
+
+    }
+    public class AppEngine
+    {
+        int val = 0;
+        Student[] s = new Student[100];
+        List<Course> courses = new List<Course>(5);
+        List<Enroll> e = new List<Enroll>();
+        Course[] c = new Course[100];
+        Enroll[] enroll = new Enroll[10];
+
+        public void introduce(Course course)
+        {
+            Console.WriteLine("how many Course data want to Insert");
+            val = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < val; i++)
+            {
+
+                Console.WriteLine("Enter Course Id");
+                course.cid = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Course Name");
+                course.name = Console.ReadLine();
+                Console.WriteLine("Enter Course Duration");
+                course.duration = Console.ReadLine();
+                Console.WriteLine("Enter Course Fees");
+                course.fee = Convert.ToSingle(Console.ReadLine());
+
+                c[i] = new Course()
+                {
+                    cid = course.cid,
+                    name = course.name,
+                    duration = course.duration,
+                    fee = course.fee,
+                };
+                
+
+            }
+        }
+
+        //        public void Introduce()
+        //{
+        //    Program.CourseRegistration();
+        //}
+
+        public void register(Student student)
+        {
+            try
+            {
+                Console.WriteLine("enter how many student data want to register\n");
+                val = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < val; i++)
+                {
+                    Console.WriteLine("Enter Student Id");
+                    student.ID = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Student Name");
+                    student.Name = Console.ReadLine();
+                    Console.WriteLine("Enter Student Dob");
+                    student.dob = DateTime.Parse(Console.ReadLine());
+                    s[i] = new Student()
+                    {
+                        ID = student.ID,
+                        Name = student.Name,
+                        dob = student.dob,
+                    };
+                }
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine("Error:  Check the Inputs which you have given");
+            }
+            
+        }
+        //public void register()
+        //{
+        //    Program.StudentRegistration();
+        //}
+
+        //public void listOfStudents()
+        //{
+        //    Program.displayStudent();
+        //   // return s;
+
+        //}
+        public Student[] listOfStudents()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < val; i++)
+            {
+                s[i] = new Student()
+                {
+                    ID = s[i].ID,
+                    Name = s[i].Name,
+                    dob = s[i].dob,
+                };
+            }
+
+
+            //    Program.displayStudent();
+
+            return s;
+
+        }
+
+        public List<Course> listOfCourses()
+        {
+
+            for (int i = 0; i < val; i++)
+            {
+                courses =(new List<Course> { 
+                          new Course{cid = courses[i].cid,name =courses[i].name, duration = courses[i].duration,fee = courses[i].fee}
+                        });
+            }
+
+            return courses;
+        }
+        //        public void listOfCourses()
+        //{
+        //    Program.displayCourse();
+        //}
+        public void Enroll(Student student, Course course)
+        {
+            Enroll e2 = new Enroll();
+            Console.WriteLine("enter how many student details needs to enroll\n NOTE: Maximum limit is 10 in a Time");
+            val = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < val; i++)
+            {
+                Console.WriteLine("Enter Student ID");
+                student.ID = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Student Name");
+                student.Name = Console.ReadLine();
+                Console.WriteLine("Enter Course Id");
+                course.cid = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Course Name");
+                course.name = Console.ReadLine();
+                e2.enrollDate = Convert.ToDateTime(DateTime.Now);//datetime.now is used to give the todays date
+                //student.AddRange(new List<Student> {
+                //   new Student { ID = id,Name = name},
+                //});
+                //course.AddRange(new List<Course> {
+                //   new Course {cid = Cid,name = CName},
+                //});
+                //e.AddRange(new List<Student> { 
+                //)};
+
+                s[i] = new Student() //  student object intiallizer
+                {
+                    ID = student.ID,
+                    Name = student.Name,
+                };
+                c[i] = new Course() //course obj initiallizer
+                {
+                    cid = course.cid,
+                    name = course.name,
+                };
+                enroll[i] = new Enroll() //enroll object initiallizer
+                {
+                    enrollDate = e2.enrollDate,
+                };
+
+
+
+
+            }
+        }
+
+        public Enroll[] listOfEnrollments()
+        {
+            for (int i = 0; i < val; i++)
+            {
+
+                enroll[i] = new Enroll()
+                {
+                    std = new Student
+                    {
+                        ID = s[i].ID,
+                        Name = s[i].Name,
+                    },
+                    crs = new Course()
+                    {
+                        cid = c[i].cid,
+                        name = c[i].name,
+                    },
+                    enrollDate = enroll[i].enrollDate,
+
+                };
+            }
+            return enroll;
+        }
+
+       
+
+       
+
+
+    }
+}
---
+++ MiniProjTeam2_1/CaseStudy3.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProjTeam2_1
+{
+    class CaseStudy3
+    {
+    }
+
+    public abstract class UserInterface
+    {
+        AppEngine a = new AppEngine();
+        Course c = new Course();
+        Student s = new Student();
+        Info i1 = new Info();
+
+        public void showFirstScreen()
+        {
+            try
+            {
+                Console.WriteLine("Welcome to the student Management App");
+            StartFirstscreen:
+                Console.WriteLine("***************************************************");
+                Console.WriteLine("select the user type option from below.\n1.Student \n2.Admin\n3.To Exit");
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        showStudentScreen();
+                        break;
+                    case 2:
+                        showAdminScreen();
+                        break;
+                    case 3:
+                        Console.WriteLine("press Enter key to exit..");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input select the correct user type to proceed");
+                        goto StartFirstscreen;
+                }
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine("Error : Invalid input type");
+                showFirstScreen();
+            }
+        }
+        public void showStudentScreen()
+        {
+
+        start:
+            try
+            {
+                Console.WriteLine("***************Student Screen****************");
+                Console.WriteLine("1.Show the list of students\n2.Show the list of Courses \n3. Exit from Student Screen\n4. Exit from Application");
+                Console.WriteLine("select the required choice of  action you want to proceed");
+                int showStudentScreeninput = Convert.ToInt32(Console.ReadLine());
+
+                switch (showStudentScreeninput)
+                {
+                    case 1:
+                        CaseStudy4.displayStudent();
+                        Console.WriteLine("---------------------------------------");
+                        break;
+                    case 2:
+                        //showAllStudentsScreen();
+                        CaseStudy4.displayCourse();
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Are you sure want to exit Y/N:");
+                        char exitVal = Convert.ToChar(Console.ReadLine().ToUpper());
+
+                        if (exitVal == 'Y')
+                        {
+                            Console.WriteLine("Exiting.....");
+                            showFirstScreen();
+                        }
+                        else if (exitVal == 'N')
+                        {
+                            // Console.WriteLine("\n");
+                            goto start;
+                        }
+
+                        break;
+                    case 4:
+                        Console.WriteLine("press enter to exit");
+                        break;
+
+                    default:
+                        Console.WriteLine("enter the any one input from the given option: ");
+                        goto start;
+                       
+                }
+
+                if (showStudentScreeninput != 3 && showStudentScreeninput !=4)
+                {
+                    goto start;
+                }
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Error: invalid input");
+                goto start;
+            }
+        }
+        public void showAdminScreen()
+        {
+        StartAdmin:
+            try
+            {
+                Console.WriteLine("\n************Admin Screen******************");
+                Console.WriteLine("\n1. Register new Course\n2.To Show the list of Course\n3.Register new Student\n4.Display Student list\n5.Update detail\n6.New Enrollment\n7.List of Enrollments\n8.Exit Admin Screen\n9.Exit Application");
+                Console.WriteLine("select the required action you want to proceed");
+                int showCourseScreeninput = Convert.ToInt32(Console.ReadLine());
+               
+                switch (showCourseScreeninput)
+                {
+                    case 1:
+                        //introduceNewCourseScreen();
+                        Console.WriteLine("*********************Course Screen*****************\n");
+                        Console.WriteLine("\nRegistered Courses");
+                        CaseStudy4.displayCourse();
+                        CaseStudy4.CourseRegistration(c);
+                        break;
+                    case 2:
+                        Console.WriteLine("*********************Course Screen*****************\n");
+                        CaseStudy4.displayCourse();
+                        break;
+                    case 3:
+                        Console.WriteLine("\nRegistered Students");
+                        CaseStudy4.StudentRegistration(s);
+                        break;
+                    case 4:
+                        CaseStudy4.displayStudent();
+                        break;
+                    case 5:
+                        Console.WriteLine("***********Update details**********\nselect the which detail you want to update:\n1.Update Student\n2.Exit from update screen\n3.Exit from Admin screen");
+                        int input = Convert.ToInt32(Console.ReadLine().ToUpper());
+                        switch (input)
+                        {
+                            case 1:
+                                CaseStudy4.displayStudent();
+                                Console.WriteLine("****************************");
+                                CaseStudy4.UpdateStudent(s);
+                                Console.WriteLine("****************************");
+                                CaseStudy4.displayStudent();
+                                break;
+                            case 2:
+                                goto StartAdmin;
+
+                            case 3:
+                                showFirstScreen();
+                                break;
+                            default:
+                                Console.WriteLine("enter valid input");
+                                goto StartAdmin;
+
+
+                        }
+                        break;
+                    case 6:
+                        Console.WriteLine("***************Enrollment screen***************");
+                        CaseStudy4.Enroll(s, c);
+                        break;
+                    case 7:
+                        Console.WriteLine("**************List of Enrolled Students*************");
+                        CaseStudy4.displayEnrollments();
+                        break;
+                    case 8:
+                        Console.WriteLine("Press enter key to exit.. from Admin Screen");
+                        showFirstScreen();
+                        break;
+                    case 9:
+                        Console.WriteLine("press enter to exit");
+                        break;
+                    default:
+                        Console.WriteLine("enter the any one input from the above option: ");
+                        goto StartAdmin;
+                        
+                }
+                if (showCourseScreeninput != 8  && showCourseScreeninput !=9)
+                {
+                    goto StartAdmin;
+                }
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine("Error: Invalid data");
+                goto StartAdmin;
+            }
+        }
+        public void showAllStudentsScreen()
+        {
+
+            CaseStudy4.displayStudent();
+           //  var studentVal = a.listOfStudents();
+
+            // int count = studentVal.Count();
+            //for (int i = 0; i < studentVal.Count(); i++)
+            //{
+
+            //    if (studentVal[i] != null)
+            //    {
+            //        i1.SDisplay(studentVal[i]);
+            //    }
+            //    else
+            //    {
+            //        showStudentScreen();
+            //    }
+               
+            //}
+
+        }
+        public void showStudentRegistrationScreen()
+        {
+            // Console.WriteLine("register std");
+
+            a.register(s);
+        }
+        public void introduceNewCourseScreen()
+        {
+            // a.introduce();
+            a.introduce(c);
+        }
+        public void showAllCoursesScreen()
+        {
+            var courseVal = a.listOfCourses();
+            for (int i = 0; i < courseVal.Count(); i++)
+            {
+                if (courseVal[i] != null)
+                {
+                    i1.SCourse(courseVal[i]);
+                }
+                else
+                {
+                    showAdminScreen();
+                }
+            }
+        }
+
+    }
+}
